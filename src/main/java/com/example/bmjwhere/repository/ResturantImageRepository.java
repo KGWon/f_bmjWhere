f75f850d8c56387dbb88ba90d526a757ef7cfc84
KGWon <93180301+KGWon@users.noreply.github.com>
2021-11-11 00:33:04 +0900

    Update ResturantImageRepository.java

--- src/main/java/com/example/bmjwhere/repository/ResturantImageRepository.java
+++ src/main/java/com/example/bmjwhere/repository/ResturantImageRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface ResturantImageRepository extends JpaRepository<ResturantImage, Long> {
     @Modifying
-    @Query("delete from ResturantImage ri where ri.resturant.rno = :rno") // 등록된 영화 삭제 전 영화 이미지들 삭제
+    @Query("delete from ResturantImage ri where ri.resturant.rno = :rno") 
     void deleteByImages(@Param("rno") Long rno);
 }
